9d8fef560cbbea8f6e75e2057f0a1243ac4e7618
mumucodes <75107280+mumucodes@users.noreply.github.com>
2021-04-28 21:38:11 -0700

    particle constructor built

--- new/app.js
+++ new/app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+const particleArray = [];
 
 window.addEventListener('resize', function(){
     canvas.width = window.innerWidth;
@@ -23,18 +24,12 @@ canvas.addEventListener('mousemove', function(e){
     mouse.y = e.y;
 });
 
-function drawCircle() {
-    ctx.fillStyle = 'red';
-    ctx.strokeStyle = 'blue';
-    ctx.lineWidth = 6;
-    ctx.beginPath();
-    ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
-    ctx.stroke();
-}
 class Particle {
-    conststructor(){
-        this.x = mouse.x;
-        this.y = mouse.y;
+    constructor(){
+        // this.x = mouse.x;
+        // this.y = mouse.y;
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
         this.size = Math.random() * 5 + 1;
         this.speedX = Math.random() * 5 - 1.5;
         this.speedY = Math.random() * 5 - 1.5;
@@ -48,13 +43,27 @@ class Particle {
         ctx.strokeStyle = 'blue';
         ctx.lineWidth = 6;
         ctx.beginPath();
-        ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
+        ctx.arc(this.x, this.y, 50, 0, Math.PI * 2);
         ctx.stroke();
     }
 }
+function init(){
+    for (let i = 0; i < 100; i++){
+        particleArray.push(new Particle());
+    }
+}
+init();
+
+function handleParticles(){
+    for (let i = 0; i < particleArray.length; i++){
+        particleArray[i].update();
+        particleArray[i].draw();
+    }
+}
+
 function animate(){
     ctx.clearRect(0,0, canvas.width, canvas.height);
-    drawCircle();
+    handleParticles();
     requestAnimationFrame(animate);
 }
 animate();
