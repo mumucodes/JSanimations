2a620de590ea90dbe8995f52c47237855175b62a
mumucodes <75107280+mumucodes@users.noreply.github.com>
2021-04-29 07:36:29 -0700

    SVG button folder setup

--- particles2/app.js
+++ particles2/app.js
@@ -4,6 +4,7 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const particleArray = [];
+let hue = 0;
 
 window.addEventListener('resize', function(){
     canvas.width = window.innerWidth;
@@ -17,53 +18,61 @@ const mouse = {
 canvas.addEventListener('click', function(e){
     mouse.x = e.x;
     mouse.y = e.y;
+    for (let i = 0; i < 20; i++){
+        particleArray.push(new Particle());
+    }
 
 });
 canvas.addEventListener('mousemove', function(e){
     mouse.x = e.x;
     mouse.y = e.y;
+    for (let i = 0; i < 10; i++);
+    particleArray.push(new Particle());
 });
 
 class Particle {
     constructor(){
-        // this.x = mouse.x;
-        // this.y = mouse.y;
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.size = Math.random() * 5 + 1;
+        this.x = mouse.x;
+        this.y = mouse.y;
+        // this.x = Math.random() * canvas.width;
+        // this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 25 + 1;
         this.speedX = Math.random() * 5 - 1.5;
         this.speedY = Math.random() * 5 - 1.5;
+        this.color = 'hsl(' + hue + '100%, 50%)';
     }
     update(){
         this.x += this.speedX;
         this.y += this.speedY;
+        if (this.size > 0.6) this.size -= 0.1;
     }
     draw() {
-        ctx.fillStyle = 'red';
-        ctx.strokeStyle = 'blue';
-        ctx.lineWidth = 6;
+        ctx.fillStyle = this.color;
         ctx.beginPath();
-        ctx.arc(this.x, this.y, 50, 0, Math.PI * 2);
-        ctx.stroke();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
     }
 }
-function init(){
-    for (let i = 0; i < 100; i++){
-        particleArray.push(new Particle());
-    }
-}
-init();
+
 
 function handleParticles(){
     for (let i = 0; i < particleArray.length; i++){
         particleArray[i].update();
         particleArray[i].draw();
+        if (particleArray[i].size <= 0.3){
+            particleArray.splice(i, 1);
+            console.log(particleArray.length);
+            i--;
+        }
     }
 }
 
 function animate(){
-    ctx.clearRect(0,0, canvas.width, canvas.height);
+    // ctx.clearRect(0,0, canvas.width, canvas.height);
+    ctx.fillStyle = 'rgba(0,0,0,0.02)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
     handleParticles();
+    hue += 0.5;
     requestAnimationFrame(animate);
 }
 animate();
